8757d7741fa7676e3860cf726a5b327529628933
Anthony Coleman Miller <mill2717@msu.edu>
2020-01-22 00:12:51 -0500

    again

--- tonymiller/src/App.js
+++ tonymiller/src/App.js
@@ -26,7 +26,7 @@ class App extends Component {
 
   getResumeData(){
     $.ajax({
-      url:'../public/resumeData.json',
+      url:'tonymiller\public\resumeData.json',
       dataType:'json',
       cache: true,
       success: function(data){
