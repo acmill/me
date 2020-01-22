c73908fc22a45aaf9b151cdedee48b6fd23bcb02
Anthony Coleman Miller <mill2717@msu.edu>
2020-01-21 23:36:40 -0500

    Updates

---
+++ precache-manifest.19c1a6637144b72956ad591071241111.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e89962605b8a7a759d8d0d966f3ef299",
+    "url": "/acmill/index.html"
+  },
+  {
+    "revision": "42e938b7b61ee2820d92",
+    "url": "/acmill/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6e887685ca6ff87d2608",
+    "url": "/acmill/static/js/2.a78117f6.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/acmill/static/js/2.a78117f6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "42e938b7b61ee2820d92",
+    "url": "/acmill/static/js/main.b862c223.chunk.js"
+  },
+  {
+    "revision": "3edf14e9bea9e3e51507",
+    "url": "/acmill/static/js/runtime-main.eb788c47.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/acmill/static/media/logo.5d5d9eef.svg"
+  }
+]);
