e95631b74a6db1f0ca1fa57d733a0743e5c23d79
Anthony Coleman Miller <mill2717@msu.edu>
2020-01-31 05:06:05 -0500

    font awesome 5

--- tonymiller/src/Components/Header.js
+++ tonymiller/src/Components/Header.js
@@ -38,18 +38,11 @@ class Header extends Component {
          <div className="banner-text">
             <h1 className="responsive-headline">I'm {name}.</h1>
             <h3>I'm a {city} based <span>{occupation}</span>. {description}.</h3>
-<<<<<<< HEAD
-=======
             <hr />            
->>>>>>> 093e2dd2d3f3e74c710ea85ddc7859da1a9fe51a
          </div>
          <ul className="social" id="nav">
                {networks}
          </ul>
-<<<<<<< HEAD
-
-=======
->>>>>>> 093e2dd2d3f3e74c710ea85ddc7859da1a9fe51a
       </div>
 
       <p className="scrolldown">
