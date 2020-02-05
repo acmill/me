073f64df0e9de9732eb44f147f6cb0e6eafed0d4
Anthony Coleman Miller <mill2717@msu.edu>
2020-02-05 01:58:25 -0500

    nav changes, addition of blog link (much excite)

--- tonymiller/src/Components/Header.js
+++ tonymiller/src/Components/Header.js
@@ -32,8 +32,7 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="#testimonials">Testimonials</a></li>
-            <li><a className="smoothscroll" href="#contact">Contact</a></li>
+            <li><a className="smoothscroll" href="https://tony-coleman.com/blog">Blog</a></li>
          </ul>
       </nav>
 
