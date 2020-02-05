739607dd82888aa3d5d93afae0a9755d60619a79
Anthony Coleman Miller <mill2717@msu.edu>
2020-02-05 02:32:05 -0500

    still doesn't work.. can't leave it

--- tonymiller/src/Components/Header.js
+++ tonymiller/src/Components/Header.js
@@ -32,7 +32,6 @@ class Header extends Component {
             <li><a className="smoothscroll" href="#about">About</a></li>
 	         <li><a className="smoothscroll" href="#resume">Resume</a></li>
             <li><a className="smoothscroll" href="#portfolio">Works</a></li>
-            <li><a className="smoothscroll" href="/blog">Blog</a></li>
          </ul>
       </nav>
 
